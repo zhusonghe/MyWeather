dac74757fb6c7d2f76510045643eae4c03305248
zhusonghe <100000001@qq.com>
2016-09-22 19:32:40 +0800

    init project

--- app/src/main/java/com/example/administrator/mini_weather/MainActivity.java
+++ app/src/main/java/com/example/administrator/mini_weather/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.administrator.mini_weather;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2016/9/20.
  */
-public class MainActivity {
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
 }
